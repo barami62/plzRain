a682f60d192b251ea44f2dc058d82a68de265856
Barami62 <wodbs125@gmail.com>
2020-06-20 22:38:19 +0900

    Add css & Add many time

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const weather = document.getElementById("weather");
 const weatherKey = 'bfebc285607853a00e5d7cb88e7045e7';
 const COORDS = 'coords';
+const CNT = 16;
 
 const degree = (deg) => {
     if(deg < 22.5 && deg >= 337.5) {
@@ -19,37 +20,50 @@ const degree = (deg) => {
         return '서풍';
     } else if(deg >= 292.5 && deg < 337.5) {
         return '북서풍';
+    } else {
+        return '무풍';
     }
 };
 
 const getWeather = (lat, lng) => {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherKey}&units=metric&lang=kr&cnt=9`
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherKey}&units=metric&lang=kr&cnt=${CNT}`
     ).then((res) => {
         return res.json();
     }).then((json) => {
         console.log(json);
         let date = new Date();
-        date.setTime(json.list[0].dt * 1000);
         
-        let weatherText = {
-            '지역': json.city.name,
-            '시간': date,
-            '기온': (json.list[0].main.temp).toFixed(1) + "°C",
-            '최고 기온': (json.list[0].main.temp_max).toFixed(1),
-            '최저 기온': (json.list[0].main.temp_min).toFixed(1),
-            '기압': json.list[0].main.pressure + "hPa",
-            '습도': json.list[0].main.humidity + "%",
-            '날씨': json.list[0].weather[0].description,
-            '풍속': json.list[0].wind.speed + "m/s",
-            '풍향': degree(json.list[0].wind.deg),
-            '구름': json.list[0].clouds.all + "%",
-        };
 
-        Object.keys(weatherText).map(key => {
-            const createP = document.createElement('p');
-            createP.textContent = `${key} : ${weatherText[key]}`
-            weather.appendChild(createP);
-        });
+        const createPOne = document.createElement('p');
+        createPOne.textContent = `지역 : ${json.city.name}`
+        weather.appendChild(createPOne);
+        
+        for(let i = 0; i < CNT; i++) {
+            date.setTime(json.list[i].dt * 1000);
+
+            let weatherText = {
+                '시간': date,
+                '기온': (json.list[i].main.temp).toFixed(1) + "°C",
+                '최고 기온': (json.list[i].main.temp_max).toFixed(1),
+                '최저 기온': (json.list[i].main.temp_min).toFixed(1),
+                '기압': json.list[i].main.pressure + "hPa",
+                '습도': json.list[i].main.humidity + "%",
+                '날씨': json.list[i].weather[0].description,
+                '풍속': json.list[i].wind.speed + "m/s",
+                '풍향': degree(json.list[i].wind.deg),
+                '구름': json.list[i].clouds.all + "%",
+            };
+
+            const createDiv = document.createElement('div');
+
+            Object.keys(weatherText).map(key => {
+                const createP = document.createElement('p');
+                createP.textContent = `${key} : ${weatherText[key]}`
+                createDiv.appendChild(createP);
+                weather.appendChild(createDiv);
+            });
+        }
+        
     });
 };
 
