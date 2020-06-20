93173ee111f0d46117315d8a5738b9efd457eea4
barami62 <wodbs7758@naver.com>
2020-06-20 16:17:33 +0900

    Delete dotenv in pasckage.json

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const indexRouter = require('./routes/index');
-const weatherRouter = require('./routes/weather');
-require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', indexRouter);
+app.use('/', indexRouter);
 
-// app.get('/', (req, res) => res.sendFile('/index.html'));
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
