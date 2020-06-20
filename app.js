d10f5db496c1f59b4e1e8bb6933d0c498cec93fc
barami62 <wodbs7758@naver.com>
2020-06-20 14:14:48 +0900

    Test Success html & js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const indexRouter = require('./routes/index');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+
+// app.get('/', (req, res) => res.sendFile('/index.html'));
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
