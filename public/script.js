6042db5e5e84178ecd271000a34c9054f53b810a
barami62 <wodbs7758@naver.com>
2020-06-20 17:15:04 +0900

    Refactoring script.js and Add wind degree

--- public/script.js
+++ public/script.js
@@ -6,26 +6,47 @@ let weatherKey = 'bfebc285607853a00e5d7cb88e7045e7';
 xhr.onload = () => {
     if (xhr.status === 200 || xhr.status === 201) {
         let weatherObj = JSON.parse(xhr.responseText);
+        console.log(weatherObj);
 
-        const currentTemp = document.createElement('p');
-        const currentHumi = document.createElement('p');
-        const currentWeather = document.createElement('p');
-        const currentWind = document.createElement('p');
-        const currentCloud = document.createElement('p');
+        let weatherText = {
+            '기온': (weatherObj.main.temp - 273.15).toFixed(1),
+            '최고 기온': (weatherObj.main.temp_max - 273.15).toFixed(1),
+            '최저 기온': (weatherObj.main.temp_min - 273.15).toFixed(1),
+            '기압': weatherObj.main.pressure + "hPa",
+            '습도': weatherObj.main.humidity + "%",
+            '날씨': weatherObj.weather[0].main,
+            '풍속': weatherObj.wind.speed + "m/s",
+            '풍향': degree(weatherObj.wind.deg),
+            '구름': weatherObj.clouds.all + "%",
+        };
 
-        currentTemp.textContent = "현재 온도 : " + (weatherObj.main.temp - 273.15).toFixed(1);
-        currentHumi.textContent = "현재 습도 : " + weatherObj.main.humidity + "%";
-        currentWeather.textContent = "현재 날씨 : " + weatherObj.weather[0].main;
-        currentWind.textContent = "현재 풍속 : " + weatherObj.wind.speed + "m/s";
-        currentCloud.textContent = "현재 구름 : " + weatherObj.clouds.all + "%";
-
-        weather.appendChild(currentTemp);
-        weather.appendChild(currentHumi);
-        weather.appendChild(currentWeather);
-        weather.appendChild(currentWind);
-        weather.appendChild(currentCloud);
+        Object.keys(weatherText).map(key => {
+            const createP = document.createElement('p');
+            createP.textContent = `${key} : ${weatherText[key]}`
+            weather.appendChild(createP);
+        });
     }
 };
 
 xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${weatherKey}`);
 xhr.send();
+
+const degree = (deg) => {
+    if(deg < 22.5 && deg >= 337.5) {
+        return '북풍';
+    } else if(deg >= 22.5 && deg < 67.5) {
+        return '북동풍';
+    } else if(deg >= 67.5 && deg < 112.5) {
+        return '동풍';
+    } else if(deg >= 112.5 && deg < 157.5) {
+        return '남동풍';
+    } else if(deg >= 157.5 && deg < 202.5) {
+        return '남풍';
+    } else if(deg >= 202.5 && deg < 247.5) {
+        return '남서풍';
+    } else if(deg >= 247.5 && deg < 292.5) {
+        return '서풍';
+    } else if(deg >= 292.5 && deg < 337.5) {
+        return '북서풍';
+    }
+};
